6ec68b339a46fc06d3f7087fbeffdafd6fee863f
SuperCarPQ <phuquocsupercar@gmail.com>
2026-08-05 17:51:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Đổi VERSION mỗi lần deploy bản mới -> máy người dùng tự nhận ra */
-const VERSION = "1.4.1";
+const VERSION = "1.5.0";
 const CACHE = `qlx-${VERSION}`;
 const SHELL = ["./", "./index.html", "./manifest.json"];
 
